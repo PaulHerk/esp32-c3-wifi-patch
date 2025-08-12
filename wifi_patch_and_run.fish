382174382facf73c770e38f083418f8d2baad71a
Kaenigsion <herklotzpaul@gmail.com>
2025-08-12 07:45:57 +0200

    changed so sanity check always returns 0

--- wifi_patch_and_run.fish
+++ wifi_patch_and_run.fish
@@ -1,4 +1,3 @@
-
 #!/opt/homebrew/bin/fish
 
 set file $argv[1]
@@ -6,14 +5,25 @@ echo "file: $file"
 
 cp $file before
 
-xxd -p $file | tr -d '\n'| grep -ob "638ad702b7a6003c" | read -d : -l offset _instructions
+# 797106d622d426d24ad04ece52cc56ca5ac885
+# 638ad702b7a6003c
+xxd -p $file | tr -d '\n'| grep -ob "797106d622d426d24ad04ece52cc56ca5ac885" | read -d : -l offset _instructions
 if test -z "$offset"
     echo "Error: Offset not found"
     exit 1
 end
+set string '\x01\x45'
 set offset (math (math $offset + 8) / 2)
 echo "Offset: $offset"
-printf '\x01\x45\x82\x80\x13\x00\x00\x00' | dd of=$file seek=$offset bs=1 conv=notrunc
+for i in (seq 1 16)
+    set string $string'\x01\x00'
+end
+set string $string'\x82\x80'
+for i in (seq 1 231)
+    set string $string'\x01\x00'
+end
+echo "String: $string"
+printf $string | dd of=$file seek=$offset bs=1 conv=notrunc
 # c.li a0, 0x0
 # ret
 # nop
